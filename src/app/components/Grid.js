109d70df92a0947ff5e4dedd474443624636e8ec
Mark J Rawlins <96557207+5F8575tree@users.noreply.github.com>
2024-02-19 22:44:03 +0900

    build the grid for all elements and make it responsive

---
+++ src/app/components/Grid.js
@@ -0,0 +1,23 @@
+import styles from "../grid.module.css";
+
+const Grid = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.profile}>profile</div>
+      <div className={styles.project1}>project 1</div>
+      <div className={styles.lightDarkMode}>light/dark mode</div>
+      <div className={styles.resume}>resume</div>
+      <div className={styles.language}>language</div>
+      <div className={styles.project2}>project 2</div>
+      <div className={styles.workTogether}>work together</div>
+      <div className={styles.linkedIn}>linkedin</div>
+      <div className={styles.gitHub}>github</div>
+      <div className={styles.project3}>project 3</div>
+      <div className={styles.project4}>project 4</div>
+      <div className={styles.skills}>skills</div>
+      <div className={styles.aboutMe}>about me</div>
+    </div>
+  )
+}
+
+export default Grid
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import styles from "./page.module.css";
 import ToggleTheme from "./components/ToggleTheme";
 import NavBar from "./components/NavBar";
+import Grid from "./components/grid";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <ToggleTheme />
       <NavBar />
+      <Grid />
     </main>
   );
 }
